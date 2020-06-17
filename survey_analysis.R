7b58f96e3501c265ab87ad57698bd9083b6bd027
Shannon Carter <skcarter25@gmail.com>
2020-06-17 15:49:44 -0500

    finished pre- post-survey plots

--- survey_analysis.R
+++ survey_analysis.R
@@ -8,14 +8,17 @@ rm(list = ls(all = T))
 
 library(tidyverse)
 library(ggstance)
+library(gridExtra)
 
 mytheme <- theme_bw(base_size = 15, base_family = "Franklin Gothic Medium") +
   theme(legend.text = element_text(size = 10),
         legend.title = element_text(size = 11),
         text = element_text(size = 14),     
         axis.title = element_text(size = 12),
-        axis.text  = element_text(size = 10, family = "Franklin Gothic Medium"),
-        panel.grid = element_blank())
+        axis.text.y  = element_text(size = 10, family = "Franklin Gothic Medium"),
+        axis.text.x  = element_text(size = 14, family = "Franklin Gothic Medium"),
+        panel.grid = element_blank(),
+        title = element_text(size = 11))
 
 df <- read.csv("survey_data.csv")
 
@@ -32,26 +35,86 @@ ggplot(post, aes(x = response)) +
   geom_histogram(stat = "count") +
   facet_wrap(~question) +
   mytheme +
+  scale_x_discrete(drop = F) +
   labs(x = NULL,
-       y = "number of responses")
+       y = "number of responses") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 ###--- DELTA PLOTS -----------------------------------------------
 
 df2 <- df %>% 
   filter(!is.na(confident_managing_data)) %>% 
   pivot_longer(cols = confident_managing_data:onboard, 
                names_to = "question", values_to = "response") %>% 
-  mutate(time = factor(time, levels = c("pre", "post"))) %>% 
-  mutate(response = factor(response, levels = c("Disagree", "Somewhat Disagree", "Neither Agree nor Disagree", "Somewhat Agree", "Agree")))
+  mutate(time = factor(time, 
+                       levels = c("pre", "post"))) %>% 
+  mutate(response = factor(response, 
+                           levels = c("Disagree", "Somewhat Disagree", "Neither Agree nor Disagree", "Somewhat Agree", "Agree")))
 
 pd <- ggstance::position_dodgev(0.3)
-plot1 <- df2 %>% 
+q1 <- df2 %>% 
   filter(question == "confident_managing_data") %>% 
   ggplot(aes(x = time, y = response, color = person, group = person)) +
-  geom_point(position = pd) + 
-  geom_line(position = pd) +
+  geom_point(position = pd, size = 2) + 
+  geom_line(position = pd, size = 1.5) +
   mytheme +
   labs(x = NULL,
        y = NULL,
        title = "I feel confident managing my data") +
-  theme(legend.position = 'none')
-plot1
+  theme(legend.position = 'none') +
+  scale_y_discrete(drop = F)
+
+q2 <- df2 %>% 
+  filter(question == "quickly_find_data") %>% 
+  ggplot(aes(x = time, y = response, color = person, group = person)) +
+  geom_point(position = pd, size = 2) + 
+  geom_line(position = pd, size = 1.5) +
+  mytheme +
+  labs(x = NULL,
+       y = NULL,
+       title = "I can quickly find my data when I need it") +
+  theme(legend.position = 'none') +
+  scale_y_discrete(drop = F)
+
+q3 <- df2 %>% 
+  filter(question == "comfortable_with_colleagues") %>% 
+  ggplot(aes(x = time, y = response, color = person, group = person)) +
+  geom_point(position = pd, size = 2) + 
+  geom_line(position = pd, size = 1.5) +
+  mytheme +
+  labs(x = NULL,
+       y = NULL,
+       title = "I feel comfortable talking to my colleagues about data") +
+  theme(legend.position = 'none') +
+  scale_y_discrete(drop = F)
+
+q4 <- df2 %>% 
+  filter(question == "best_practices") %>% 
+  ggplot(aes(x = time, y = response, color = person, group = person)) +
+  geom_point(position = pd, size = 2) + 
+  geom_line(position = pd, size = 1.5) +
+  mytheme +
+  labs(x = NULL,
+       y = NULL,
+       title = "I understand best practices on where to store data") +
+  theme(legend.position = 'none') +
+  scale_y_discrete(drop = F)
+
+q5 <- df2 %>% 
+  filter(question == "eto") %>% 
+  ggplot(aes(x = time, y = response, color = person, group = person)) +
+  geom_point(position = pd, size = 2) + 
+  geom_line(position = pd, size = 1.5) +
+  mytheme +
+  labs(x = NULL,
+       y = NULL,
+       title = "I understand how to add new data to ETO") +
+  theme(legend.position = 'none') +
+  scale_y_discrete(drop = F)
+
+q1
+q2
+q3
+q4
+q5
+grid.arrange(q1, q2, q4, q5)
