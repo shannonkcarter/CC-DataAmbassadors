356a541634fd583c37ef859ae177a228eebd6ed2
Shannon Carter <skcarter25@gmail.com>
2021-05-25 11:55:42 -0500

    finished wesely survey images

--- wesley_survey_viz.R
+++ wesley_survey_viz.R
@@ -5,29 +5,55 @@ library(googlesheets4)
 mytheme <- theme_bw(base_size = 15, base_family = "Franklin Gothic Medium") +
   theme(panel.grid = element_blank())
 
-pre <- read_sheet("https://docs.google.com/spreadsheets/d/1HMe_zjXTHrhgKWQmx_r2s9WLx-36MpdSRXW7NCBBPUw/edit#gid=1402504997")
-post <- read_sheet("https://docs.google.com/spreadsheets/d/1WeBHfjruh99MxddPPmgE2roPruvjGOuR7-4N4b35Pfc/edit?resourcekey#gid=1474246294")
+pre_raw <- read_sheet("https://docs.google.com/spreadsheets/d/1HMe_zjXTHrhgKWQmx_r2s9WLx-36MpdSRXW7NCBBPUw/edit#gid=1402504997")
+post_raw <- read_sheet("https://docs.google.com/spreadsheets/d/1WeBHfjruh99MxddPPmgE2roPruvjGOuR7-4N4b35Pfc/edit?resourcekey#gid=1474246294")
 
-df <- pre %>% 
+###--- PRE SURVEY HISTOGRAM ----------------------------------------
+pre <- pre_raw %>% 
   select(-c("Timestamp", "Email Address", `What are some of the top challenges you face on a day to day basis around data?`)) %>% 
   pivot_longer(-Name, names_to = "question", values_to = "response") %>% 
   mutate(question = str_remove_all(question, "]"),
          question = str_remove_all(question, "\\[")) %>% 
   mutate(response = factor(response, levels = c("Disagree", "Somewhat Disagree", "Neither Agree nor Disagree", "Somewhat Agree", "Agree"), ordered = T))
 
-ggplot(df, aes(x = response)) +
+ggplot(pre, aes(x = response)) +
   geom_histogram(stat = "count", fill = "#50ABAB") +
-  facet_wrap(~question,  labeller = label_wrap_gen(width = 40)) +
+  facet_wrap(~question,  labeller = label_wrap_gen(width = 50)) +
   mytheme +
   scale_x_discrete(drop = F) +
   labs(x = NULL,
        y = "number of responses") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.background = element_rect(color="white", fill="white"),
+        strip.text = element_text(size = 12, color = "black", hjust = 0)
+        )
 ggsave("wesley_presurvey.jpg", width = 8, height = 8, dpi = 600)
 
+###--- POST SURVEY HISTOGRAM -------------------------------------
+post <- post_raw %>% 
+  select(2:9) %>% 
+  pivot_longer(-Name, names_to = "question", values_to = "response") %>% 
+  mutate(question = str_remove_all(question, "]"),
+         question = str_remove_all(question, "\\["),
+         question = str_remove_all(question, "Compared with how I felt at the beginning of this program... ")) %>% 
+  mutate(response = factor(response, levels = c("Disagree", "Somewhat Disagree", "Neither Agree nor Disagree", "Somewhat Agree", "Agree"), ordered = T)) %>% 
+  filter(question != "Overall, this program was valuable to me.")
 
+ggplot(post, aes(x = response)) +
+  geom_histogram(stat = "count", fill = "#50ABAB") +
+  facet_wrap(~question,  labeller = label_wrap_gen(width = 45)) +
+  mytheme +
+  scale_x_discrete(drop = F) +
+  labs(x = NULL,
+       y = "number of responses",
+       title = "Compared with how I felt at the beginning of this program...") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.background = element_rect(color="white", fill="white"),
+        strip.text = element_text(size = 12, color = "black", hjust = 0)
+  )
+ggsave("wesley_postsurvey.jpg", width = 11, height = 8, dpi = 600)
 ###--- DELTA PLOTS -----------------------------------------------
-pre_df <- pre %>% 
+pre_df <- pre_raw %>% 
   select(Name, 4:7) %>% 
   rename(name = 1, 
          `I feel comfortable using data to measure program outcomes` = 2,
@@ -36,7 +62,7 @@ pre_df <- pre %>%
          `It's easy to keep track of clients who are referred from one program to another within Wesley` = 5) %>% 
   mutate(time = "pre")
 
-post_df <- post %>% 
+post_df <- post_raw %>% 
   select(2, 3, 5, 6, 8) %>%
   rename(name = 1, 
        `I feel comfortable using data to measure program outcomes` = 2,
@@ -45,9 +71,13 @@ post_df <- post %>%
        `It's easy to keep track of clients who are referred from one program to another within Wesley` = 5) %>% 
   mutate(time = "post") %>% 
   rbind(pre_df) %>% 
-  select(name, time, everything())
+  select(name, time, everything()) %>% 
+  # some people reported their names differently at pre/post. this selects just first name
+  mutate(name = sub(" .*", "", name)) %>% 
+  # remove people who only took one of the 2 surveys
+  filter(!name %in% c("Bonnie", "David", "Dulce"))
   
-df2 <- post_df %>% 
+pre_post <- post_df %>% 
   pivot_longer(cols = 3:6, 
                names_to = "question", values_to = "response") %>% 
   mutate(time = factor(time, 
@@ -56,13 +86,20 @@ df2 <- post_df %>%
                            levels = c("Disagree", "Somewhat Disagree", "Neither Agree nor Disagree", "Somewhat Agree", "Agree")))
 
 pd <- ggstance::position_dodgev(0.3)
-qs <- df2 %>% 
+qs <- pre_post %>% 
   ggplot(aes(x = time, y = response, color = name, group = name)) +
   geom_point(position = pd, size = 2) +
   geom_line(position = pd, size = 1.5) +
-  facet_wrap(~question, labeller = label_wrap_gen()) +
-  mytheme +
+  facet_wrap(~question, labeller = label_wrap_gen(width = 45)) +
+  scale_color_manual(values = c("#22162B", "#451F55", "#724E91", "#E54F6D", "#F8C630", "#E3170A", "#50ABAB")) +
+  #scale_color_brewer(palette = "Dark2") +
   labs(x = NULL, y = NULL) +
-  theme(legend.position = "none") +
+  mytheme +
+  theme(legend.position = "none",
+    strip.background = element_rect(color="white", fill="white"),
+    strip.text = element_text(size = 12, color = "black", hjust = 0),
+    axis.text = element_text(size = 12)
+  ) +
   scale_y_discrete(drop = F)
 qs
+ggsave("wesley_pre_post_survey.jpg", width = 9, height = 8, dpi = 600)
